ffb142a9f23a1aeb3534f33db4367c3b4f4b1506
Rabiori <69498994+rabiori88@users.noreply.github.com>
2020-11-12 13:56:29 +0900

    Video Detail 페이지 추가

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-Hello, My Friends  
-Thank you for having interest in this repository ! 
 
-To use this application, 
+Youtube Clone 프로그램
 
-1. make dev.js file inside config folder 
-2. put mongoDB info into dev.js file 
-3. Type  " npm install " inside the root directory  ( Download Server Dependencies ) 
-4. Type " npm install " inside the client directory ( Download Front-end Dependencies )
-
-
-If you have problem, feel free to ask me ^^ 
-
-You can watch the tutorial for this app.
-
-https://www.youtube.com/channel/UCFyXA9x8lpL3EYWeYhj4C4Q?view_as=subscriber
+- 랜딩페이지 작업 완료
 
 
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,17 +1,19 @@
-import React, {useEffect} from 'react'
-import {Row, Col, List} from 'antd'
+import React, {useEffect, useState} from 'react'
+import {Row, Col, List, Avatar} from 'antd'
 import Axios from 'axios'
 
-function VideoDetailPage() {
+function VideoDetailPage(props) {
 
 
-    const videoId = this.props.match.params.videoId
+    const [VideoDetail, setVideoDetail] = useState([])
+    const videoId = props.match.params.videoId
     const variable = {videoId: videoId}
 
     useEffect(() => {
         Axios.post('/api/video/getVideoDetail', variable)
         .then(response => {
             if(response.data.success) {
+                setVideoDetail(response.data.videoDetail)
 
             } else {
                 alert("비디오 데이터를 가져오는데 실패")
@@ -19,37 +21,44 @@ function VideoDetailPage() {
         })
     }, [])
 
+    if(VideoDetail.writer) {
+        return (
+            <div>
+                <Row gutter={[16,16]}>
+                    <Col lg={18} xs={24}>
+    
+                        <div style={{width: '100%', padding:'3rem 4rem'}}>
+                            <video style={{width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
+    
+                            <List.Item actions >
+    
+                                <List.Item.Meta 
+                                avatar={<Avatar src={VideoDetail.writer.image} /> }
+                                title={VideoDetail.writer.name}
+                                description={VideoDetail.description}
+                                /> 
+                                
+                            </List.Item>
+    
+                            {/* {Comments} */}
+    
+                        </div>
+    
+                    </Col>
+                    <Col lg={6} xs={24}>
+                        Side Video
+                    </Col>
+                </Row>
+                
+            </div>
+        )
+    } else {
+        return(
+            <div>... loading </div>
+        )
+    }
 
-    return (
-        <div>
-            <Row gutter={[16,16]}>
-                <Col lg={18} xs={24}>
-
-                    <div style={{width: '100%', padding:'3rem 4rem'}}>
-                        <vido style={{width: '100%' }} src controls />
-
-                        <List.Item actions >
-
-                            <List.Item.Meta 
-                            avatar
-                            title
-                            description
-                            />
-                            
-                        </List.Item>
-
-                        {/* {Comments} */}
-
-                    </div>
-
-                </Col>
-                <Col lg={6} xs={24}>
-                    Side Video
-                </Col>
-            </Row>
-            
-        </div>
-    )
+    
 }
 
 export default VideoDetailPage
